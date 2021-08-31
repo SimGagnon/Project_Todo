418bcfcf822314aaa6f370102c23d8b88aefbf85
Simon Gagnon <gagnonbsimon@gmail.com>
2021-08-31 16:22:47 -0400

    added delete animations

--- app/js/script.js
+++ app/js/script.js
@@ -2,9 +2,12 @@ const addButton = document.querySelector(".todo__form button");
 const todoInput = document.querySelector(".todo__form input");
 const form = document.querySelector("form");
 const listParent = document.querySelector(".todo__list")
+const todoFilter = document.querySelectorAll(".todo__nav li")
 
 form.addEventListener("submit", addTodo);
-
+todoFilter.forEach(li => {
+    li.addEventListener('click', filterTodo);
+})
 
 function addTodo(e) {
     e.preventDefault();
@@ -28,7 +31,6 @@ function addTodo(e) {
     toDoDelete.innerHTML = '<ion-icon name="trash-outline"></ion-icon>';
     toDo.appendChild(toDoDelete);
 
-
     todoInput.value = "";
     listParent.appendChild(toDo);
 }
@@ -37,8 +39,40 @@ function addTodo(e) {
 function deleteCheck(e) {
     const item = e.target
     //Delete todo 
-    item.classList[0] === 'todo__button--delete' ? item.parentNode.remove() : null ;
-
+ /*    item.classList[0] === 'todo__button--delete' ? item.parentNode.remove() : null ; */
+    if(item.classList[0] === 'todo__button--delete') {
+        item.parentNode.classList.add("slideout");
+        item.parentNode.addEventListener('transitionend', function() {
+            item.parentNode.remove()
+        })
+    }
     // Check todo
     item.classList[0] === 'todo__button--check' ? item.parentNode.classList.toggle('completed') : null;
+}
+
+function filterTodo(e) {
+
+    const todos = listParent.childNodes;
+    console.log(todos);
+    todos.forEach(function(todo) {
+        switch(e.target.value){
+            case "all":
+                todo.style.display = "flex";
+                break;
+            case "completed":
+                if(todo.classList.contains("completed")) {
+                    todo.style.display = "flex";
+                } else {
+                    todo.style.display = "none";
+                } 
+                break;
+            case "active":
+                if(!todo.classList.contains("completed")) {
+                    todo.style.display = "flex";
+                } else {
+                    todo.style.display = "none";
+                } 
+                break;
+        }
+    })
 }
